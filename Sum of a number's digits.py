97601521f9b6185e3f90caca1cf8ef695a689dc5
cjenkins2468 <115499734+cjenkins2468@users.noreply.github.com>
2022-11-03 10:34:37 -0400

    Update Sum of a number's digits.py

--- Sum of a number's digits.py
+++ Sum of a number's digits.py
@@ -1,11 +1,10 @@
+def get_sum_of_numers_digits(n):  #defining the function
 '''
 author: Charlie
 bugs: none
 '''
-def get_sum_of_numers_digits(n):
-
-    n = float(n)
-    if n<10:
-        return n
+    n = float(n)                    #making the variable n into float
+    if n<10:                        #if statement true if n is less than 10
+        return n                    #returning the n if ture
     else:
-        return get_sum_of_numers_digits(n/10) + n %10
+        return get_sum_of_numers_digits(n/10) + n %10       #sum of numbers loop here (the % is the mod)
