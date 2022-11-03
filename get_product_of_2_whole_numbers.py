b23e34307881ac9be40a3143134083ad45d1b7aa
cjenkins2468 <115499734+cjenkins2468@users.noreply.github.com>
2022-11-03 10:38:57 -0400

    Update get_product_of_2_whole_numbers.py

--- get_product_of_2_whole_numbers.py
+++ get_product_of_2_whole_numbers.py
@@ -1,16 +1,11 @@
-def get_product_of_2_whole_numbers(a,b):
-    b = int(b)
+def get_product_of_2_whole_numbers(a,b):      #defining function
+'''
+author: Charlie
+bugs: none
+'''
+    b = int(b)                      # making the everything an int
     a = int(a)
-    if b > 0:
-        return a + get_product_of_2_whole_numbers(a,b-1)
+    if b > 0:                   #if statement
+        return a + get_product_of_2_whole_numbers(a,b-1)   # making a 2 whole numbers loop
     if b == 0:
-        return 0
-
-def main():
-
-    b = input("b = ")
-    a = input("a = ")
-    end = get_product_of_2_whole_numbers(a,b)
-    print (end) 
-
-main()
+        return 0            #if true return 0
