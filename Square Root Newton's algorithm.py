d2d02e32fef3b84cd7e1981ef5847825cee20ec8
cjenkins2468 <115499734+cjenkins2468@users.noreply.github.com>
2022-11-03 10:32:36 -0400

    Update Square Root Newton's algorithm.py

--- Square Root Newton's algorithm.py
+++ Square Root Newton's algorithm.py
@@ -1,9 +1,9 @@
-def get_square_root(n,p,e):
-    n = float(n)
+def get_square_root(n,p,e):                 #defining the function 
+    n = float(n)                            #making them all floats
     p = float(p)
     e = float(e)
-    if1 = (e*2)-2
-    if abs(if1) < p:
-        return e
+    if1 = (e*2)-2                           #doing the job so I dont have to in the if statement to make it more orginized
+    if abs(if1) < p:                        # if statement with absolute value inside
+        return e                            # returning only e if it is tru
     else:
-        return get_square_root(n,p,(((e+n)/e)/2))
+        return get_square_root(n,p,(((e+n)/e)/2))     #doing the square root loop
